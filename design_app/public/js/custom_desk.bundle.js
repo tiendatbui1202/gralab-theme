406f568240532264940c02d69118dd11849db8c6
tiendatbui <tiendatbui2015@gmail.com>
2025-10-05 00:32:13 +0700

    update theme

--- design_app/public/js/custom_desk.bundle.js
+++
@@ -1,3 +0,0 @@
-import "./frappe/ui/toolbar/navbar.html";
-import "./frappe/list/list_sidebar.html";
-import "./frappe/ui/page.html";
--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from setuptools import setup, find_packages
+import re, ast
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in design_app/__init__.py
-from design_app import __version__ as version
+# get version from __version__ variable in kard_theme/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('newara/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
-	name="design_app",
+	name='newara',
 	version=version,
-	description="Design App",
-	author="Umer",
-	author_email="farooqx2560@gmail.com",
+	description='Custom Frappe Theme',
+	author='H@LFWARE',
+	author_email='contact@half-ware.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
